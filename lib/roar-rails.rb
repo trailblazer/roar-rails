69803b1714f9f620a2e2f735b77fa355d710aebd
Nick Sutterer <apotonick@gmail.com>
2012-01-04 20:16:49 +0100

    opinionated loading of frequently used files.

--- lib/roar-rails.rb
+++ lib/roar-rails.rb
@@ -1,5 +1,9 @@
 require "roar/rails/version"
 
+# Load frequently used files. 
+require "roar/representer/json"
+require "roar/representer/feature/hypermedia"
+
 module Roar
   module Rails
     # Your code goes here...
