59d8f36090c2578b0a6cc94f2d5476548e5159e2
Nick Sutterer <apotonick@gmail.com>
2012-01-06 14:12:40 +0100

    added a default return value to #default_url_options to prevent this: https://github.com/apotonick/roar-rails/issues/1

--- lib/roar/rails/url_methods.rb
+++ lib/roar/rails/url_methods.rb
@@ -3,7 +3,7 @@
 module Roar::Rails
   module UrlMethods
     def default_url_options
-      Rails.application.config.representer.default_url_options
+      Rails.application.config.representer.default_url_options or {}
     end
   end
 end
--- test/representer_test.rb
+++ test/representer_test.rb
@@ -8,6 +8,16 @@ class RepresenterTest < ActionController::TestCase
   test "representers can use URL helpers" do
     get :show, :id => "bumi"
     assert_body "{\"name\":\"Bumi\",\"links\":[{\"rel\":\"self\",\"href\":\"http://http://roar.apotomo.de/singers/Bumi\"}]}"
-
+  end
+  
+  test "it works with uninitialized config.representer.default_url_options" do
+    url_options = Rails.application.config.representer.default_url_options
+    Rails.application.config.representer.default_url_options = nil
+    
+    assert_raises ArgumentError do
+      get :show, :id => "bumi"
+    end
+    
+    Rails.application.config.representer.default_url_options = url_options
   end
 end
