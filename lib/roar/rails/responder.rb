61aefa99f4465bba2893000ab4d5c133cd166d67
Niklas Cathor <niklas.cathor@loxi.biz>
2012-02-23 13:01:35 +0100

    Fixed Roar::Rails::Responder: * options -> given_options * pass representer to extend_with_representer in collection as well as single resource * don't call to_hash if it's not implemented

--- lib/roar/rails/responder.rb
+++ lib/roar/rails/responder.rb
@@ -5,13 +5,14 @@ module Roar::Rails
       resource.extend(representer)
     end
     def display(resource, given_options={})
+      representer = given_options.delete(:with_representer)
       if resource.respond_to?(:map!)
         resource.map! do |r|
-          extend_with_representer!(r)
-          r.to_hash
+          extend_with_representer!(r, representer)
+          r.respond_to?(:to_hash) ? r.to_hash : r
         end
       else
-        extend_with_representer!(resource, options.delete(:with_representer))
+        extend_with_representer!(resource, representer)
       end
       super
     end
