62d22cff7309599103f76c6281cf47f4394a71bc
Timo Schilling <timo@schilling.io>
2013-03-01 10:19:06 +0100

    change routes of test app for rails 4, remove deprecated match with get

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,5 +1,5 @@
 Dummy::Application.routes.draw do
-  match ':controller(/:action(/:id(.:format)))'
+  get ':controller(/:action(/:id(.:format)))'
   root :to => 'musician#index'
   resources :singers
 end
