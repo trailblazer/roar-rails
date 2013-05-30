6d7dd0c8b7bd6a785bfe21c9872e0ae0ef9b0ea7
Nick Sutterer <apotonick@gmail.com>
2013-05-30 16:38:16 +1000

    cleanup Responder::display for @goldstar.

--- lib/roar/rails/responder.rb
+++ lib/roar/rails/responder.rb
@@ -1,18 +1,21 @@
 module Roar::Rails
   module Responder
     def display(model, *args)
-      if representer = options.delete(:represent_items_with)
-        render_items_with(model, representer) # convenience API, not recommended since it's missing hypermedia.
-        return super
-      end
+      handle_lonely_collection(model) and return super # :represent_items_with
 
       model = prepare_model_for(format, model, options)
-
-      super
+      super # AC::Responder: this calls controller.render which calls renderer[:json] which calls model.to_json.
     end
 
   private
-    def render_items_with(collection, representer)
+    def handle_lonely_collection(collection)
+      return false unless representer = options.delete(:represent_items_with)
+
+      setup_items_with(collection, representer) # convenience API, not recommended since it's missing hypermedia.
+      true
+    end
+
+    def setup_items_with(collection, representer)
       collection.map! do |mdl|  # DISCUSS: i don't like changing the method argument here.
         representer.prepare(mdl).to_hash(options) # FIXME: huh? and what about XML?
       end
