7957a4a1e683f17055f619a48dc6f3f4d2d986ee
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-07-30 14:55:53 -0300

    Helper method for representer paths

--- test/representer_generator_test.rb
+++ test/representer_generator_test.rb
@@ -11,16 +11,17 @@ class RepresentetGeneratorTest < Rails::Generators::TestCase
   test "create a representer with correct class_name" do
     run_generator %w(singer)
 
-    assert_file "app/representers/singer_representer.rb",
-      /module SingerRepresenter/
+    assert_file representer_path('singer'), /module SingerRepresenter/
   end
 
   test "create a representer with correct properties" do
     run_generator %w(singer name id)
 
-    assert_file "app/representers/singer_representer.rb",
-      /property :name/
-    assert_file "app/representers/singer_representer.rb",
-      /property :id/
+    assert_file representer_path('singer'), /property :name/
+    assert_file representer_path('singer'), /property :id/
+  end
+
+  def representer_path(name)
+    "app/representers/#{name}_representer.rb"
   end
 end
