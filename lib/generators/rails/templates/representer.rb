e680273e08632eed89dc22f7dfbd613a8985a5dd
Anthony Kirwan <amkirwan@gmail.com>
2014-09-23 23:57:09 -0500

    fix include indent spacing

--- lib/generators/rails/templates/representer.rb
+++ lib/generators/rails/templates/representer.rb
@@ -1,9 +1,9 @@
 module <%= class_name %>Representer
-  <% if format == 'JSON::JsonApi' %>
-    include Roar::JSON::JsonApi
-  <% else %>
-    include Roar::Representer::<%= format %>
-  <% end %>
+<% if format == 'JSON::JsonApi' %>
+  include Roar::JSON::JsonApi
+<% else %>
+  include Roar::Representer::<%= format %>
+<% end %>
   <% property_options.each do |property| %>
   <%= property -%>
   <% end %>
