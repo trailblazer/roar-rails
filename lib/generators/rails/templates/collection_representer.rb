b6744d3ac874ee5188dcf030151fae2c51b6d506
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-09-29 17:50:23 -0600

    Add module namespacing to representers

--- lib/generators/rails/templates/collection_representer.rb
+++ lib/generators/rails/templates/collection_representer.rb
@@ -1,5 +1,7 @@
+<% module_namespacing do -%>
 module <%= class_name.pluralize %>Representer
   include Roar::Representer::<%= format %>::Collection
 
   items extend: <%= class_name %>Representer, class: <%= class_name %>
 end
+<% end %>
--- lib/generators/rails/templates/representer.rb
+++ lib/generators/rails/templates/representer.rb
@@ -1,6 +1,8 @@
+<% module_namespacing do -%>
 module <%= class_name %>Representer
   include Roar::Representer::<%= format %>
   <% property_options.each do |property| %>
   <%= property -%>
   <% end %>
 end
+<% end -%>
