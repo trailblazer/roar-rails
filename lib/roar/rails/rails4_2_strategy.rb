675d7593902253250df6706565232abd3cbfd1e6
Eric Skogen <eskogen@audionerd.com>
2014-12-30 11:57:19 -0600

    remove unused Rails 4.1 strategy cleanup Rails 4.2 strategy

--- lib/roar/rails/rails4_2_strategy.rb
+++ lib/roar/rails/rails4_2_strategy.rb
@@ -3,10 +3,6 @@ require 'action_controller/responder'
 module Roar::Rails
   class Responder < ActionController::Responder
     module VersionStrategy
-      extend ActiveSupport::Concern
-      included do
-        # TODO (see rails_4_1_strategy.rb)
-      end
     end
   end
 
