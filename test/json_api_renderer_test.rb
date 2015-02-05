364758a9cff57f080dd1b1a4b256ca2093190327
Chad W. Taylor <github@chadwtaylor.com>
2015-02-04 16:30:27 -0800

    Update json_api_renderer_test.rb

--- test/json_api_renderer_test.rb
+++ test/json_api_renderer_test.rb
@@ -3,33 +3,51 @@ require 'test_helper'
 class HalRendererTest < ActionController::TestCase
   include Roar::Rails::TestCase
 
-  class SingersController < ActionController::Base
-    module JsonApiSingerRepresenter
+  class PeopleController < ActionController::Base
+    module PersonRepresenter
       include Roar::JSON::JSONAPI
-
-      type :singer
-      property :name
+      type :people
+      property :first_name
     end
 
     include Roar::Rails::ControllerAdditions
-    represents :json_api, :entity => JsonApiSingerRepresenter
+
+    represents :json_api, :entity => PersonRepresenter
 
     def show
-      singer = Musician.new("Bumi")
-      respond_with singer
+      person = Person.find 1
+      respond_with person
     end
+
+    def index
+      people = Person.find([1,2])
+      respond_with people, represent_items_with:PersonRepresenter
+    end
+
   end
 
-  tests SingersController
+  tests PeopleController
 
-  test "should render correctly in response to a application/vnd.api+json" do
-    get :show, :id => "bumi", :format => :json_api
-    assert_body '{"singer":{"name":"Bumi"}}'
+  test "should render single model correctly in response to a application/vnd.api+json" do
+    get :show, :id => "1", :format => :json_api
+    # assert_body '{"people":{"first_name":"Chad"}}'
+    assert_equal response.body, '{"people":{"first_name":"Chad"}}'
   end
 
-  test "should have a content_type of application/vnd.api+json" do
+  test "should render collection of models correctly in response to a application/vnd.api+json" do
+    get :index, :format => :json_api
+    # assert_body '{"people":[{"first_name":"Chad"},{"first_name":"Fremont"}]}'
+    assert_equal response.body, '{"people":[{"first_name":"Chad"},{"first_name":"Fremont"}]}'
+  end
+
+  test "should have a content_type of application/vnd.api+json for a single model" do
     get :show, :id => "bumi", :format => :json_api
     assert_equal response.content_type, 'application/vnd.api+json'
   end
-end
 
+  test "should have a content_type of application/vnd.api+json for a collection of models" do
+    get :index, :format => :json_api
+    assert_equal response.content_type, 'application/vnd.api+json'
+  end
+
+end
