47421a3b22b4d40ecf55872cfdc44699dfffe720
Alex Coles <alex@alexbcoles.com>
2017-02-25 20:10:05 +0000

    Monkey-patch Roar JSON API Document module
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- lib/roar/rails/json_api.rb
+++ lib/roar/rails/json_api.rb
@@ -1,7 +1,7 @@
 require 'roar/json/json_api'
 
 # FIXME: Hack. Add another module to Roar JSON API to hook into.
-Roar::JSON::JSONAPI::Defaults.class_eval do
+Roar::JSON::JSONAPI::Document.class_eval do
   def to_json_api(*args);   to_json(*args);   end
   def from_json_api(*args); from_json(*args); end
 end
